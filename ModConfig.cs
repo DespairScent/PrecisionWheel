1d01db49accebfe6f2506c5305e75aede7b72f83
DespairScent <74109497+DespairScent@users.noreply.github.com>
2024-04-10 21:06:54 +0300

    Add config

---
+++ ModConfig.cs
@@ -0,0 +1,21 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespairScent.PrecisionWheel
+{
+    internal class ModConfig
+    {
+        public KeybindList keybind1 = new KeybindList();
+        public KeybindList keybind10 = new KeybindList(SButton.LeftShift);
+        public KeybindList keybind100 = new KeybindList(SButton.LeftControl);
+
+        public bool keybindInheritance = true;
+
+        public bool reverseWheel = false;
+    }
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -11,21 +11,82 @@ namespace DespairScent.PrecisionWheel
     internal sealed class ModEntry : Mod
     {
 
+        private static ModConfig Config;
+
         private static bool movingWithWheel = false;
 
         public override void Entry(IModHelper helper)
         {
+            Config = helper.ReadConfig<ModConfig>();
+
             var harmony = new Harmony(this.ModManifest.UniqueID);
 
             harmony.Patch(AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.rightClick)),
                transpiler: new HarmonyMethod(typeof(ModEntry), nameof(SpecialTitlePatch)));
 
             helper.Events.Input.MouseWheelScrolled += this.OnWheelScrolled;
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
         }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+            {
+                return;
+            }
+
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config)
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => "Toogle keybind (x1)",
+                tooltip: () => "A button that must be held down to allow one item to be moved with the mouse wheel",
+                getValue: () => Config.keybind1,
+                setValue: value => Config.keybind1 = value
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => "Toogle keybind (x10)",
+                tooltip: () => "A button that must be held down to allow 10 items to be moved with the mouse wheel",
+                getValue: () => Config.keybind10,
+                setValue: value => Config.keybind10 = value
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => "Toogle keybind (x100)",
+                tooltip: () => "A button that must be held down to allow 100 items to be moved with the mouse wheel",
+                getValue: () => Config.keybind100,
+                setValue: value => Config.keybind100 = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Keybind inheritance",
+                tooltip: () => "Determines whether it is necessary to hold down the keybinds for fewer items to activate the current",
+                getValue: () => Config.keybindInheritance,
+                setValue: value => Config.keybindInheritance = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Reverse wheel",
+                tooltip: () => "Reverses wheel direction",
+                getValue: () => Config.reverseWheel,
+                setValue: value => Config.reverseWheel = value
+            );
+        }
+
 
         private void OnWheelScrolled(object? sender, MouseWheelScrolledEventArgs e)
         {
-            if (e.Delta != 0 && Game1.activeClickableMenu is ItemGrabMenu)
+            if (e.Delta != 0 && Game1.activeClickableMenu is ItemGrabMenu && GetCountFromKeybins() != 0)
             {
                 var itemGrabMenu = (ItemGrabMenu)Game1.activeClickableMenu;
                 Game1.PushUIMode();
@@ -35,7 +96,7 @@ namespace DespairScent.PrecisionWheel
 
                 if (hoverItem != null) // player inventory
                 {
-                    if (e.Delta > 0)
+                    if (e.Delta > 0 ^ Config.reverseWheel)
                     {
                         Game1.activeClickableMenu.receiveRightClick(Game1.getMouseX(), Game1.getMouseY());
                     }
@@ -47,7 +108,7 @@ namespace DespairScent.PrecisionWheel
                 else // storage
                 {
                     hoverItem = itemGrabMenu.ItemsToGrabMenu.getItemAt(Game1.getMouseX(), Game1.getMouseY());
-                    if (e.Delta < 0)
+                    if (e.Delta < 0 ^ Config.reverseWheel)
                     {
                         Game1.activeClickableMenu.receiveRightClick(Game1.getMouseX(), Game1.getMouseY());
                     }
@@ -134,21 +195,53 @@ namespace DespairScent.PrecisionWheel
                 return false;
             }
 
-            int count = 1;
-            if (Game1.isOneOfTheseKeysDown(Game1.oldKBState, new InputButton[] { new InputButton(Keys.LeftShift) }))
+            int count = Math.Min(inventory.actualInventory[index].Stack, GetCountFromKeybins());
+            if (count == 0)
             {
-                count = 10;
-                if (Game1.isOneOfTheseKeysDown(Game1.oldKBState, new InputButton[] { new InputButton(Keys.LeftControl) }))
-                {
-                    count = 100;
-                }
+                return false;
             }
-            count = Math.Min(inventory.actualInventory[index].Stack, count);
 
             one.Stack = count;
             inventory.actualInventory[index].Stack -= count;
 
             return true;
+        }
+
+        private static int GetCountFromKeybins()
+        {
+            int count = 0;
+            if (Config.keybindInheritance)
+            {
+                if (Config.keybind1.Keybinds.Length == 0 || Config.keybind1.IsDown())
+                {
+                    count = 1;
+                    if (Config.keybind10.Keybinds.Length == 0 || Config.keybind10.IsDown())
+                    {
+                        count = 10;
+                        if (Config.keybind10.Keybinds.Length == 0 || Config.keybind100.IsDown())
+                        {
+                            count = 100;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Config.keybind100.Keybinds.Length == 0 || Config.keybind100.IsDown())
+                {
+                    count = 100;
+                }
+                else if (Config.keybind10.Keybinds.Length == 0 || Config.keybind10.IsDown())
+                {
+                    count = 10;
+                }
+                else if (Config.keybind1.Keybinds.Length == 0 || Config.keybind1.IsDown())
+                {
+                    count = 1;
+                }
+            }
+
+            return count;
         }
 
     }
